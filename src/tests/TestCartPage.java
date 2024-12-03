5e5eeb4491694c20bda176a4e5f8768690ebf7f9
Fariha Hoque Mimma <49908132+farihahoque@users.noreply.github.com>
2024-12-03 09:18:30 +0600

    Add files via upload

---
+++ src/tests/TestCartPage.java
@@ -0,0 +1,62 @@
+import io.appium.java_client.AppiumBy;
+import io.appium.java_client.android.AndroidDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.testng.annotations.AfterSuite;
+import org.testng.annotations.BeforeSuite;
+import org.testng.annotations.Test;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.time.Duration;
+
+public class TestCartPage {
+    public AndroidDriver driver;
+
+    @BeforeSuite
+    public void TestApp() throws MalformedURLException {
+        DesiredCapabilities desiredCapabilities = new DesiredCapabilities();
+        desiredCapabilities.setCapability("appium:deviceName", "pixel1");
+        desiredCapabilities.setCapability("platformName", "android");
+        desiredCapabilities.setCapability("appium:automationName", "UiAutomator2");
+        desiredCapabilities.setCapability("udid", "emulator-5554");
+        desiredCapabilities.setCapability("appium:appPackage", "com.androidsample.generalstore");
+        desiredCapabilities.setCapability("appium:appActivity", "com.androidsample.generalstore.SplashActivity");
+        URL remoteUrl = new URL("http://127.0.0.1:4723");
+
+        driver = new AndroidDriver(remoteUrl, desiredCapabilities);
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+
+        var el1 = driver.findElement(AppiumBy.id("android:id/text1"));
+        el1.click();
+        var el2 = driver.findElement(AppiumBy.androidUIAutomator("new UiScrollable(new UiSelector().scrollable(true).instance(0)).scrollIntoView(new UiSelector().text(\"Bangladesh\"))"));
+        el2.click();
+        var el3 = driver.findElement(AppiumBy.id("com.androidsample.generalstore:id/nameField"));
+        el3.click();
+        el3.sendKeys("fara");
+        el3.click();
+        var el4 = driver.findElement(AppiumBy.id("com.androidsample.generalstore:id/radioFemale"));
+        el4.click();
+        var el5 = driver.findElement(AppiumBy.id("com.androidsample.generalstore:id/btnLetsShop"));
+        el5.click();
+        var el6 = driver.findElement(AppiumBy.androidUIAutomator("new UiSelector().text(\"Air Jordan 1 Mid SE\")"));
+        el6.click();
+        var el7 = driver.findElement(AppiumBy.androidUIAutomator("new UiSelector().resourceId(\"com.androidsample.generalstore:id/productAddCart\").instance(1)"));
+        el7.click();
+
+
+    }
+    @Test
+    public void CartTest() {
+        var el1 = driver.findElement(AppiumBy.id("com.androidsample.generalstore:id/appbar_btn_cart"));
+        el1.click();
+        var el2 = driver.findElement(AppiumBy.className("android.widget.Button"));
+        el2.click();
+
+    }
+    @AfterSuite
+    public void closeapp(){
+        driver.removeApp("General-Store.apk");
+        driver.quit();
+    }
+
+}
---
+++ src/tests/TestFirstPage.java
@@ -0,0 +1,56 @@
+
+import io.appium.java_client.AppiumBy;
+import io.appium.java_client.android.AndroidDriver;
+import io.appium.java_client.appmanagement.BaseOptions;
+import org.openqa.selenium.By;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.testng.annotations.AfterSuite;
+import org.testng.annotations.BeforeSuite;
+import org.testng.annotations.Test;
+
+import java.io.File;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.time.Duration;
+import java.util.Map;
+
+
+public class TestFirstPage {
+    public AndroidDriver driver;
+
+    @BeforeSuite
+    public void TestApp() throws MalformedURLException {
+        DesiredCapabilities desiredCapabilities = new DesiredCapabilities();
+        desiredCapabilities.setCapability("appium:deviceName", "pixel1");
+        desiredCapabilities.setCapability("platformName", "android");
+        desiredCapabilities.setCapability("appium:automationName", "UiAutomator2");
+        desiredCapabilities.setCapability("udid", "emulator-5554");
+        desiredCapabilities.setCapability("appium:appPackage", "com.androidsample.generalstore");
+        desiredCapabilities.setCapability("appium:appActivity", "com.androidsample.generalstore.SplashActivity");
+        URL remoteUrl = new URL("http://127.0.0.1:4723");
+
+        driver = new AndroidDriver(remoteUrl, desiredCapabilities);
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+    }
+    @Test
+    public void FirstPageTest() {
+        var el1 = driver.findElement(AppiumBy.id("android:id/text1"));
+        el1.click();
+        var el2 = driver.findElement(AppiumBy.androidUIAutomator("new UiScrollable(new UiSelector().scrollable(true).instance(0)).scrollIntoView(new UiSelector().text(\"Bangladesh\"))"));
+        el2.click();
+        var el3 = driver.findElement(AppiumBy.id("com.androidsample.generalstore:id/nameField"));
+        el3.click();
+        el3.sendKeys("fara");
+        el3.click();
+        var el4 = driver.findElement(AppiumBy.id("com.androidsample.generalstore:id/radioFemale"));
+        el4.click();
+        var el5 = driver.findElement(AppiumBy.id("com.androidsample.generalstore:id/btnLetsShop"));
+        el5.click();
+    }
+    @AfterSuite
+    public void closeapp(){
+        driver.removeApp("General-Store.apk");
+        driver.quit();
+    }
+}
+
